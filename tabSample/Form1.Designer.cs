6463df58bdfee9da0bbf0b5733efd7e2bc9e9918
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-11-30 20:52:59 +0200

    Добавьте файлы проекта.

---
+++ tabSample/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace tabSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.comboBox_queryText = new System.Windows.Forms.ComboBox();
+            this.button_startQuery = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(16, 43);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(772, 395);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(764, 369);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(434, 227);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(301, 120);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // comboBox_queryText
+            // 
+            this.comboBox_queryText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox_queryText.FormattingEnabled = true;
+            this.comboBox_queryText.Location = new System.Drawing.Point(16, 13);
+            this.comboBox_queryText.Name = "comboBox_queryText";
+            this.comboBox_queryText.Size = new System.Drawing.Size(677, 24);
+            this.comboBox_queryText.TabIndex = 1;
+            // 
+            // button_startQuery
+            // 
+            this.button_startQuery.Location = new System.Drawing.Point(699, 14);
+            this.button_startQuery.Name = "button_startQuery";
+            this.button_startQuery.Size = new System.Drawing.Size(89, 23);
+            this.button_startQuery.TabIndex = 2;
+            this.button_startQuery.Text = "Exec";
+            this.button_startQuery.UseVisualStyleBackColor = true;
+            this.button_startQuery.Click += new System.EventHandler(this.button_startQuery_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(598, 169);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_startQuery);
+            this.Controls.Add(this.comboBox_queryText);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox comboBox_queryText;
+        private System.Windows.Forms.Button button_startQuery;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ tabSample/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabSample
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection myConn = null;
+        SqlDataAdapter dataAdapter = null;
+        DataSet dataSet = null;
+        SqlCommandBuilder commandBuilder = null;
+        string connString = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            myConn = new SqlConnection();
+            connString = ConfigurationManager.ConnectionStrings["ConnStringSampleTabControl"].ConnectionString;
+            myConn.ConnectionString = connString;
+
+
+
+        }
+
+        private void button_startQuery_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(comboBox_queryText.Text))
+            {
+                if (!comboBox_queryText.Items.Contains(comboBox_queryText.Text))
+                    comboBox_queryText.Items.Add(comboBox_queryText.Text);
+            }
+            
+
+            try
+            {
+                SqlConnection myConn = new SqlConnection(connString);
+                dataSet = new DataSet();
+                string queryText = comboBox_queryText.Text;
+                dataAdapter = new SqlDataAdapter(queryText, myConn);
+                dataGridView1.DataSource = null;
+                commandBuilder = new SqlCommandBuilder(dataAdapter);
+                dataAdapter.Fill(dataSet, "table_1");
+                dataGridView1.DataSource = dataSet.Tables["table_1"];
+
+
+            }
+            catch (Exception ex) {  }
+            finally { }
+
+
+
+
+
+
+
+
+        }
+    }
+}
